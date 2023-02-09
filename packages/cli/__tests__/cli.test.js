e67d07459f358fc4024458d0fafa6e92b86b88d3
fuqiang <823342479@qq.com>
2023-02-09 16:19:58 +0800

    feat: 集成jest自动化测试

--- packages/cli/__tests__/cli.test.js
+++ packages/cli/__tests__/cli.test.js
@@ -1,3 +1,13 @@
-test("run error command", () => {
-  expect(1 + 1).toBe(2);
+import path from "node:path";
+import { execa } from "execa";
+
+const CLI = path.join(__dirname, "../bin/cli.js");
+const bin =
+  () =>
+  (...args) =>
+    execa(CLI, args);
+
+test("run error command", async () => {
+  const { stderr } = await bin()("iii");
+  console.log(stderr);
 });
