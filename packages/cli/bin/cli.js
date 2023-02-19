e6ea0e1e432073732a5f71445751b314287ead7c
付强 <fuqiang1@CN0214003515M.lan>
2023-02-19 13:55:47 +0800

    feat: change

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import importLocal from "import-local";
+// import importLocal from "import-local";
 import { filename } from "dirname-filename-esm";
 import { log } from "@cli-henry/utils";
 import entry from "../lib/index.js";
 const __filename = filename(import.meta);
-if (importLocal(__filename)) {
-  log.info("cli", "使用本地版本");
-} else {
-  entry(process.argv.slice(2));
-}
+// if (importLocal(__filename)) {
+//   log.info("cli", "使用本地版本");
+// } else {
+entry(process.argv.slice(2));
+// }
